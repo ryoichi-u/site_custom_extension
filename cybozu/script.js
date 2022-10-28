0d027375fd70e895e44ef454ec2d7193e4ffa6e9
ryoichi-u <ueno.ryoichi@lancers.co.jp>
2022-10-28 11:16:13 +0900

    feat: 評価面談アプリの横幅の拡張を実装

--- cybozu/script.js
+++ cybozu/script.js
@@ -2,10 +2,6 @@
 
 ! function(){
 
-  // Cボタンでコメントトグル
-  var button = document.getElementsByClassName('CommentOnOffButton')[0];
-  document.addEventListener('keydown', functin(e){
-    if( e.keyCode == 67 ) { button.click(); }
-  });
+  console.log('custom_extension working !');
 
 }();
